25f9e9e40c746cd1f45ce591ea30c2e4ba13168a
Denis Masinovic <denis.masinovic@gmail.com>
2018-07-24 22:47:21 +0200

    Updated core.input documentation

--- src/views/api/core/InputPage.jsx
+++ src/views/api/core/InputPage.jsx
@@ -12,18 +12,13 @@ function FieldPage() {
   return (
     <ApiPage
       title="Input"
-      description="Defines a form line containing a label and an input. Additionally it can render an info button
-      and info alert as well as validation messages. If the user adds the required validator then it
+      description="Defines a form line containing a label and an input. Additionally it will render
+      validation messages. If the user adds the required validator then it
       will mark the field as required as well."
       codeExample={introExample}
       parent="core"
     >
       <h2>Properties</h2>
-      <PropInfo name="info" type="String">
-        Message id of the info text that will be displayed along the input. If you don't
-        want to use any i18n features you can pass a raw message instead.
-      </PropInfo>
-
       <PropInfo name="type" type="String" defaultValue="text">
         HTML5 input type of the input element.
       </PropInfo>
