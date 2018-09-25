ff9269ff4ce8e71f63bca27482fa31eaf1191df3
Denis Masinovic <denis.masinovic@gmail.com>
2018-09-25 21:36:04 +0200

    Migrated PropInfo to typescript

---
+++ src/views/api/shared/PropInfo/PropInfo.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+
+// tslint:disable-next-line:import-name
+import PrismCode from 'react-prism';
+
+interface IPropInfoProps {
+  name: string;
+  required: boolean;
+  deprecated: boolean;
+  example?: string;
+  // tslint:disable-next-line:no-reserved-keywords
+  type?: string;
+  defaultValue?: string;
+  description?: string;
+}
+
+export class PropInfo extends React.Component<IPropInfoProps> {
+  // tslint:disable-next-line:typedef
+  public static defaultProps = {
+    required: false,
+    deprecated: false,
+  };
+
+  public render(): JSX.Element {
+    const {
+      name,
+      type,
+      defaultValue,
+      description,
+      required,
+      deprecated,
+      example,
+      children,
+    } = this.props;
+
+    return (
+      <div className="prop-info">
+        <h5 id={name}>
+          {name}
+          {deprecated ? <span className="text-danger font-italic"> deprecated</span> : null}
+          {required || type || defaultValue ? <br /> : null}
+          {required ? <span className="text-danger font-italic">Required </span> : null}
+          {type ? <span className="text-info font-weight-normal font-italic">{type}</span> : null}
+          {defaultValue ? <span className="text-info font-weight-normal font-italic">, default: {defaultValue}</span> : null}
+        </h5>
+        <div className="meta">
+          {description ? <p>{description}</p> : ''}
+          {children}
+          {example ? <PrismCode className="lang-jsx" component="pre">{example}</PrismCode> : ''}
+        </div>
+      </div>
+    );
+  }
+}
